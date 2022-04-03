029a3ad7d8c32e8732c489026b5367f847a2f141
jarvmds <jessicaavarella@hotmail.com>
2022-04-03 20:30:16 -0300

    atualização de dados do usuário e cálculo do IMC

--- Program.cs
+++ Program.cs
@@ -30,28 +30,28 @@ namespace CalculadoraIMC
                 Console.WriteLine("You're under the weight!!");
             }
 
-            else if (imc >= 18.50 && < 25.00)
+            else if (imc >= 18.50 && imc < 25.00)
             {
                 Console.WriteLine("Your weight is normal!!");
             }
 
 
-            else if (imc >= 25.00 && <30.00)
+            else if (imc >= 25.00 && imc < 30.00)
             {
                 Console.WriteLine("You're above the weight!!");
             }
 
-            else if (imc >= 30.00 && 35.00)
+            else if (imc >= 30.00 && imc < 35.00)
             {
                 Console.WriteLine("You're in Obesity I");
             }
 
-            else if (imc >= 35.00 && 40.00)
+            else if (imc >= 35.00 && imc < 40.00)
             {
                     Console.WriteLine("You're in Obesity II");
             }
 
-            else if (imc >= 40.00)
+            else
             {
                 Console.WriteLine("You're in Obesity III");
             }
